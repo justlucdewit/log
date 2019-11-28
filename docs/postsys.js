449091f56a3127fe7b553def72632c6607b22c33
Luc de wit <luc.dewit@hotmail.nl>
2019-11-28 21:59:59 +0100

    worked on the JSON parser

--- docs/postsys.js
+++ docs/postsys.js
@@ -1,10 +1,33 @@
-let data;
+let pageEl = document.getElementById("contentSpace");
 
-$(document).ready(function(){
-	//retrieve json
-	$.getJSON("posts.json", function(json){
-		data=json
-	});
+function createPost(title, content){
+    //create main post div
+    let postEl = document.createElement("div");
 
-	$('#contentSpace').html("Page is under construction");
+    //create and append title
+    let titleEl = document.createElement("h2");
+    titleEl.innerText = title;
+    postEl.appendChild(titleEl);
+
+    for (para of content){
+        //create and append content
+        let contentEl = document.createElement("h3");
+        contentEl.innerHTML = para;
+        postEl.appendChild(contentEl);
+    }
+    
+    //create and append divider
+    postEl.appendChild(document.createElement("hr"));
+
+
+    //append main post div to page
+    pageEl.appendChild(postEl)
+}
+
+fetch("./posts.json").then((resp)=>{
+    return resp.json();
+}).then((data)=>{
+    for (const post of data){
+        createPost(post["title"], post["paragraphs"])
+    }
 });
