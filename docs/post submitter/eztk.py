c20abf7067f117caf43b53ff0dfad296ba5620c6
Luc de wit <luc.dewit@hotmail.nl>
2019-11-29 23:02:52 +0100

    updated blog and app

--- docs/post submitter/eztk.py
+++ docs/post submitter/eztk.py
@@ -8,6 +8,8 @@ class Window(Frame):
         root = Tk()
         Frame.__init__(self, root)
         self.master = root
+        self.bgcol="#FFFFFF"
+        self.fgcol="#000000"
         self.pack(fill=BOTH, expand=1)
 
     def set_size(self, width, height):
@@ -18,22 +20,30 @@ class Window(Frame):
 
     def set_background(self, r, g, b):
         col = hexFromRgb(r, g, b)
+        self.bgcol = col
         self.configure(bg=col)
+
+    def set_foreground(self, r, g, b):
+        col = hexFromRgb(r, g, b)
+        self.fgcol = col
 
     def launch(self):
         self.master.mainloop()
 
     def createLabel(self, xpos, ypos, txt=""):
-        pass
+        lbl = Label(self, text=txt, bg=self.bgcol, fg=self.fgcol)
+        lbl.place(x=xpos, y=ypos)
+        self.update()
+        return eztk_Label(lbl.winfo_width(), lbl.winfo_height(), xpos, ypos)
 
     def createButton(self, xpos, ypos, txt=""):
-        btn = Button(self, text=txt)
+        btn = Button(self, text=txt, bg=self.bgcol, fg=self.fgcol)
         btn.place(x=xpos, y=ypos)
         self.update()
         return eztk_Button(btn.winfo_width(), btn.winfo_height(), xpos, ypos)
 
     def createInput(self, xpos, ypos, lineWidth, lines):
-        txt = Text(self, height=lines, width=lineWidth)
+        txt = Text(self, height=lines, width=lineWidth, bg=self.bgcol, fg=self.fgcol)
         txt.place(x=xpos, y=ypos)
         self.update()
         return eztk_Input(txt.winfo_width(), txt.winfo_height(), xpos, ypos)
@@ -47,6 +57,16 @@ class eztk_Input:
 
     def show(self):
         print(f"Input[x:{self.x}, y:{self.y}, width:{self.width}, height:{self.height}]")
+
+class eztk_Label:
+    def __init__(self, width, height, x, y):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f"Label[x:{self.x}, y:{self.y}, width:{self.width}, height:{self.height}]")
 
 class eztk_Button:
     def __init__(self, width, height, x, y):
--- docs/post submitter/poster.py
+++ docs/post submitter/poster.py
@@ -4,8 +4,14 @@ app = Window()
 app.set_size(600, 750)
 app.set_title("Lukes programming log submitter")
 app.set_background(0, 0, 0)
+app.set_foreground(0, 255, 0)
 
-app.createLabel()
-titleInp = app.createInput(20, 20, 50, 1)
+title_label = app.createLabel(20, 20, "Title: ")
+title_input = app.createInput(title_label.width+title_label.x + 20, 20, 50, 1)
+
+body_label = app.createLabel(20, title_input.y+title_input.height+20, "Content: ")
+body_input = app.createInput(20, body_label.y+body_label.height+20, 70, 30)
+
+submit_button = app.createButton(20, body_input.y+body_input.height+20, "Submit")
 
 app.launch()
