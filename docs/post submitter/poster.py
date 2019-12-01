2b4bc33e5b87a1d6d6a715f054f223376d660022
Luc de wit <luc.dewit@hotmail.nl>
2019-12-01 14:53:54 +0100

    updated app

--- docs/post submitter/poster.py
+++ docs/post submitter/poster.py
@@ -1,4 +1,5 @@
 from eztk import *
+from datetime import datetime
 
 app = Window()
 app.set_size(600, 750)
@@ -10,8 +11,26 @@ app.set_cursorColor(0, 255, 0)
 def submit():
     title = title_input.retrieveInput()
     content = body_input.retrieveInput()
-    print(title)
-    print(content)
+    post = {"date":"", "title":"", "paragraphs": []}
+    now = datetime.now()
+    post["date"] = f"{now.day}-{now.month}-{now.year}"
+    post["title"] = title
+
+    currentPara = ""
+    contentArr = []
+    for line in content.splitlines():
+        if line == "\n":
+            contentArr.append(currentPara)
+        else:
+            currentPara = currentPara + line
+
+    if not currentPara == "" or not currentPara == "\n":
+        contentArr.append(currentPara)
+    print(contentArr)
+    post["paragraphs"] = contentArr
+    print(post)
+    # print(title)
+    # print(content)
 
 title_label = app.createLabel(20, 20, "Title: ")
 title_input = app.createInput(title_label.width+title_label.x + 20, 20, 50, 1)
